8414a2b7f0c0c49975a97994016bac56896eb5d7
david nyong <davydevelops@gmail.com>
2023-12-05 11:35:28 +0100

    hello pushing

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,3 +1,4 @@
+
 const PrismaClient = require("@prisma/client").PrismaClient;
 
 const prisma = new PrismaClient();
@@ -21,7 +22,7 @@ const seed = async () => {
       publishedTitle: "This is a published title",
       slug: "lead-magnet-slug",
       status: "draft",
-      userId: "user_2Z1ejieeNvceauFHy4jUk8h6GQU",
+      userId: "user_2Z1ejieeNvceauFHy4jUk8h6GQUf",
     },
   });
 
@@ -31,13 +32,13 @@ const seed = async () => {
         name: "Dummy User 1",
         email: "dummy1@gmail.com",
         leadMagnetId: "123456789",
-        userId: "user_2Z1ejieeNvceauFHy4jUk8h6GQU",
+        userId: "user_2Z1ejieeNvceauFHy4jUk8h6GQUf",
       },
       {
         name: "Dummy User 2",
         email: "dummy2@gmail.com",
         leadMagnetId: "123456789",
-        userId: "user_2Z1ejieeNvceauFHy4jUk8h6GQU",
+        userId: "user_2Z1ejieeNvceauFHy4jUk8h6GQUf",
       },
     ],
   });
